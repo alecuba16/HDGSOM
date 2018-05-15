177e48ceaf937d34f635ab034b48e66190cf8b82
Alex <alecuba16@users.noreply.github.com>
2018-05-15 15:17:53 +0200

    Update print.gsom.r

--- R/print.gsom.r
+++ R/print.gsom.r
@@ -8,18 +8,18 @@ print.gsom <- function(x, ...){
   
   if(!is.null(x$mapped)){
     observations = length(x$mapped$bmn)
-    return(cat("GrowingSOM object.", observations, "observations are mapped onto an existing GSOM map.\n"))
+    return(cat("HDGSOM object.", observations, "observations are mapped onto an existing GSOM map.\n"))
   }
   
   if(!is.null(x$prediction)){
     observations = length(x$prediction$bmn)
-    return(cat("GrowingSOM object. For", observations, "observations Y values were predicted according to an existing gsom object.\n"))
+    return(cat("HDGSOM object. For", observations, "observations Y values were predicted according to an existing gsom object.\n"))
   }
   
   if(x$nhood == "rect") topology = "rectangular"
   else topology = "hexagonal"
   
-  cat("GrowingSOM (gsom) map with", nrow(x$nodes$position), "nodes and a", topology, "topology.\n")
+  cat("HDGSOM (gsom) map with", nrow(x$nodes$position), "nodes and a", topology, "topology.\n")
   
   if(!is.null(x$nodes$predict)) cat("Model contains predictions for dependent variables.\n")
   
